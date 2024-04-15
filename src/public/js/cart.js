b759a91d9ac45ab65f59f1d72fcb7b930a6689f1
dstrn <dstrn825@gmail.com>
2024-04-15 12:11:21 +0200

    cart js

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -1,14 +1,18 @@
-// Supprimer les éléments du panier
+document.querySelectorAll("#delete-item").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    console.log("delete");
+    let productId = event.target.dataset.id;
 
-document.querySelectorAll('#delete-item').forEach(button => {
-    button.addEventListener('click', event => {
-        console.log('delete');
-        let productId = event.target.dataset.id;
-        let index = cart.indexOf(productId);
-        cart.splice(index, 1);
-        setCartCookie(cart);
-        updateCart();
-    }
-    );
-}
-);
+    let cart = document.cookie
+      .split(";")
+      .find((cookie) => cookie.trim().startsWith("cart="))
+      .valueOf()
+      .split("=")[1];
+
+    cart = JSON.parse(cart);
+    cart.splice(cart.indexOf(productId), 1);
+
+    document.cookie = `cart=${JSON.stringify(cart)}; path=/`;
+    location.reload();
+  });
+});
